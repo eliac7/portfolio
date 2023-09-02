c1f963aa5645fd2c4ec753c0552cdcb2f19c4afd
Ilias Thalassochoritis <elias.thal7@gmail.com>
2023-09-02 17:28:03 +0300

    INIT

---
+++ components/about.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export default function About() {
+  return (
+    <motion.section
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+    >
+      <h2 className="mb-8 text-3xl font-medium capitalize">About me</h2>
+      <p className="mb-3">
+        Upon completing my degree in {""}
+        <span className="font-medium">Computer Engineering</span>
+        from the University of Thessaly, I was drawn to the world of web
+        development. I specialize in the{" "}
+        <span className="font-medium">MERN stack</span>, with a strong affinity
+        for <span className="font-medium">React.js</span> and{" "}
+        <span className="font-medium">Next.js</span>. The evolving nature of{" "}
+        <span className="italic">frontend development</span>, particularly in
+        HTML, CSS, and JavaScript, keeps me constantly engaged.
+      </p>
+
+      <p>
+        <span className="italic">Outside of coding</span>, I'm deeply intrigued
+        by the wonders of <span className="font-medium">IoT</span> and express
+        my creativity through <span className="font-medium">photography</span>.{" "}
+      </p>
+    </motion.section>
+  );
+}
