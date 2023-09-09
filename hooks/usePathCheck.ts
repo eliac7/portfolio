97018a6d9a0d991f74ee607fb6aab6a758e3354b
Ilias Thalassochoritis <elias.thal7@gmail.com>
2023-09-09 21:15:40 +0300

    DEBUG: Debug path check for vercel

--- hooks/usePathCheck.ts
+++ hooks/usePathCheck.ts
@@ -1,6 +1,8 @@
 import { headers } from "next/headers";
 
 export function usePathCheck(specificRoute: string): boolean {
+  console.log("usePathCheck: specificRoute", specificRoute);
+
   const headersList = headers();
   const pathname = headersList.get("x-invoke-path") || "";
   return pathname === specificRoute;
